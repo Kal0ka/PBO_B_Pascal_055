dcf7c67a7b2bc351caf4a6f26036dd6345cbc75a
Kal0ka <pasalkaloka79@gmail.com>
2025-03-07 10:45:56 +0700

    Codelab 1

---
+++ Codelab/Codelab 1 Modul 2/MainC1M2.java
@@ -0,0 +1,11 @@
+public class MainC1M2 {
+    public static void main (String[] args) {
+        Hewan hewan1 = new Hewan("Kucing","Mamalia","Nyaan~~");
+        Hewan hewan2 = new Hewan("Anjing","Mamalia","Woof~~");
+        hewan1.TampilkanInfo();
+        hewan2.TampilkanInfo();
+
+
+
+    }
+}
